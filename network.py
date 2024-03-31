ad2c31eef8534f6e0dec837c59e15efd7d9381f3
Dylan Berndt <33700799+GooseChair@users.noreply.github.com>
2024-03-31 12:01:09 -0500

    Initial commit

---
+++ network.py
@@ -0,0 +1,83 @@
+
+import tensorflow as tf
+import keras
+from keras import layers
+
+import os
+from PIL import Image
+import numpy as np
+
+import random
+
+
+def loadLabels(path: str):
+    file = open(path + 'class_dict.csv')
+    labels = []
+    for line in file.readlines()[1:]:
+        data = line.split(',')
+        labels.append([data[0], [int(data[1]), int(data[2]), int(data[3])]])
+    return labels
+
+
+def loadDataset(path: str, dataName: str, batchSize: int, classLabels: list[str, list[int]]):
+    imageDir = path + dataName
+    labelDir = imageDir + '_labels'
+
+    imagePaths = os.listdir(imageDir)
+    labelPaths = os.listdir(labelDir)
+
+    images = []
+    labels = []
+
+    zipped = list(zip(imagePaths, labelPaths))
+    random.shuffle(zipped)
+    imagePaths, labelPaths = zip(*zipped)
+
+    for i in range(batchSize):
+        imageData = Image.open(imageDir + '/' + imagePaths[i])
+        labelData = Image.open(labelDir + '/' + labelPaths[i])
+
+        image = np.array(imageData) / 255.0
+
+        labelData = np.array(labelData)
+        label = np.zeros([image.shape[0], image.shape[1], len(classLabels)], dtype=np.uint8)
+
+        for index, color in enumerate(classLabels):
+            mask = np.all(labelData == color[1], axis=2)
+            label[:, :, index] = mask
+
+        images.append(image)
+        labels.append(label)
+
+        print(dataName, str(i + 1) + '/' + str(batchSize), end='\r')
+
+    return np.array(images), np.array(labels)
+
+
+def recursiveUNet(parent: keras.Layer, shape: list[int], kernelSize: list[int], depth: int):
+    conv = layers.Conv2D(shape[-1], kernelSize, activation=keras.activations.relu, padding='same')(parent)
+    pool = layers.MaxPool2D()(conv)
+
+    if depth > 1:
+        middle = recursiveUNet(pool, shape, kernelSize, depth - 1)
+    else:
+        middle = pool
+
+    upSampled = layers.UpSampling2D()(middle)
+    conv2 = layers.Conv2D(shape[-1], kernelSize, activation=keras.activations.relu, padding='same')(upSampled)
+    concatenated = layers.Concatenate()([conv, conv2])
+
+    return concatenated
+
+
+def createUNet(shape: list[int, None], kernelSize: list[int], depth: int, classes: int):
+    inputLayer = layers.Input(shape)
+    network = recursiveUNet(inputLayer, shape, kernelSize, depth)
+
+    output = layers.Conv2D(classes, kernelSize, padding='same')(network)
+
+    model = keras.Model(inputs=inputLayer, outputs=output)
+
+    return model
+
+
